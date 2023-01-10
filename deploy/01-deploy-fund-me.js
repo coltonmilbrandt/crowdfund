4bc0db047b03919d544427b990fffae7cc06566d
coltonmilbrandt <coltonmilbrandt@gmail.com>
2023-01-10 13:42:26 -0700

    added deploy scripts and verify

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,6 +1,31 @@
+const {networkConfig, developmentChains} = require("../helper-hardhat-config")
+const {network} = require("hardhat")
+const {verify} = require("../utils/verify")
+// const ethUsdPriceFeedAddress = networkConfig[network.name]["ethUsdPriceFeed"]
+
 // export Hardhat Runtime Environment
 module.exports = async ({getNamedAccounts, deployments}) => {
     const {deploy, log} = deployments
     const {deployer} = await getNamedAccounts()
-    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if(developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [args], // price feed address by chain
+        log: true,
+    })
+
+    if(!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(fundMe.address, args)
+    }
+
+    log("-----------------------------------------------------")
 }
+module.exports.tags = ["all", "fundme"]
